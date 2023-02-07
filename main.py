c31b4dd0dbae1666da6cc3703747dd8e2dfd2ea1
Jima <jimavictor1@gmail.com>
2023-02-07 07:34:08 -0800

    procedure successful

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld(Resource):
